303da8338615719e8834ceb689f3d5f1ad084d5c
Archie Newman <archienewman09@gmail.com>
2021-12-11 17:32:09 +0000

    types updates

--- ArchieConsole/Program.cs
+++ ArchieConsole/Program.cs
@@ -4,9 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Dad is epic");
-            Thread.Sleep(3000);
-            Console.WriteLine("Archie is good"); 
+            // native variable types - https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/built-in-types
+            int sleepTime = 3000;
+            bool isDadEpic = true;
+            char firstCharacter = 'A';
+
+            if (isDadEpic)
+            {
+                Console.WriteLine("Dad is epic");
+            }
+            else
+            {
+                Console.WriteLine("Dad is not epic :(");
+            }
+
+            string firstName = "Archie";
+            Console.WriteLine("First Name: " + firstName);
+
+            Thread.Sleep(sleepTime);
+            Console.WriteLine("Archie is good");
         }
     }
 }
