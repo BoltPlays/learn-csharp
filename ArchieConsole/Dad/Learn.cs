51200313dc06876041e0b8c5c9b0ae63feab0b3f
Archie Newman <archienewman09@gmail.com>
2021-12-18 12:12:24 +0000

    Namespaces

---
+++ ArchieConsole/Dad/Learn.cs
@@ -0,0 +1,100 @@
+namespace ArchieConsole.Dad
+{
+    class Learn
+    {
+        // variable types - https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/built-in-types
+        public static void BooleanTypes()
+        {
+            // boolean - true or false (also think of this as 0 and 1) (think of as on and off)
+            bool isDadEpic = true;
+            if (isDadEpic)
+            {
+                Console.WriteLine("Dad is epic");
+            }
+            else
+            {
+                Console.WriteLine("Dad is not epic :(");
+            }
+
+            bool nothing = false;
+            Console.WriteLine(nothing);
+        }
+
+        public static void StringTypes()
+        {
+            // string - think of them as text values
+            string firstName = "Archie";
+            Console.WriteLine("First Name: " + firstName);
+            Console.WriteLine();
+
+            string pizza = "3";
+            Console.WriteLine(pizza);
+        }
+
+       public static void IntegerTypes()
+        {
+            // integers - think of them as numbers
+            int sleepTime = 200;
+            Thread.Sleep(sleepTime);
+            Console.WriteLine("Archie is good");
+            Console.WriteLine();
+
+            int anything = 4444;
+            Console.WriteLine(anything);
+        }
+
+        public static void Arrays()
+        {
+            // arrays
+            int[] numbers = new int[5];
+            numbers[0] = 12;
+            Console.WriteLine("First value in numbers array " + numbers[0]);
+            Console.WriteLine("Second value in numbers array " + numbers[1]);
+            Console.WriteLine("Third value in numbers array " + numbers[2]);
+            Console.WriteLine();
+
+            string[] food = new string[5];
+            food[0] = "Pizza";
+            food[1] = "Hot Sauce";
+            Console.WriteLine("First value in names array " + food[0]);
+            Console.WriteLine(food[1]);
+
+            string[] namesArray = { "Archie", "Charlie", "Dad", "Mum", "Grandad" };
+            // position or index        0          1        2      3       4
+            Console.WriteLine("Second string in array: " + namesArray[1]);
+            Console.WriteLine("Fifth string in array: " + namesArray[4]);
+            Console.WriteLine();
+
+            bool[] boolArray = { true, false, true };
+        }
+
+        public static void ForEachLoops()
+        {
+            string[] namesArray = { "Archie", "Charlie", "Dad", "Mum", "Grandad" };
+            foreach (string name in namesArray)
+            {
+                Console.WriteLine("name: " + name);
+            }
+
+            // 1. create a new array of 3 integers
+            // 2. assign the values of all 3 integers
+            // 3. write to the console the values of all 3 integers - using a foreach loop
+
+            int[] water = { 1, 52, 634, 23223, 99 };
+
+            foreach (int liquid in water)
+            {
+                Console.Write(liquid + " ");
+            }
+
+            // the new keyword creates a new reference in memory
+            string[] dadArray = new string[5];
+            string[] archieArray = dadArray; // archieArray is a reference to the same memory location as dadArray
+
+            dadArray[0] = "Cheeeeeeeese";
+            Console.WriteLine(archieArray[0]);
+            archieArray[1] = "Beans";
+            Console.WriteLine(dadArray[1]);
+        }
+    }
+}
--- ArchieConsole/Program.cs
+++ ArchieConsole/Program.cs
@@ -1,100 +1,23 @@
-﻿namespace ArchieConsole
+﻿using ArchieConsole.Dad;
+
+namespace ArchieConsole
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // variable types - https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/built-in-types
-            booleanTypes();
-            stringTypes();
-            integerTypes();
-
-            arrays();
-
-            forEachLoops();
-        }
-
-        private static void booleanTypes()
-        {
-            // boolean - true or false (also think of this as 0 and 1) (think of as on and off)
-            bool isDadEpic = true;
-            if (isDadEpic)
+            foreach (string argValue in args)
             {
-                Console.WriteLine("Dad is epic");
-            }
-            else
-            {
-                Console.WriteLine("Dad is not epic :(");
-            }
-        }
-
-        private static void stringTypes()
-        {
-            // string - think of them as text values
-            string firstName = "Archie";
-            Console.WriteLine("First Name: " + firstName);
-            Console.WriteLine();
-        }
-
-        private static void integerTypes()
-        {
-            // integers - think of them as numbers
-            int sleepTime = 200;
-            Thread.Sleep(sleepTime);
-            Console.WriteLine("Archie is good");
-            Console.WriteLine();
-        }
-
-        private static void arrays()
-        {
-            // arrays
-            int[] numbers = new int[5];
-            numbers[0] = 12;
-            Console.WriteLine("First value in numbers array " + numbers[0]);
-            Console.WriteLine("Second value in numbers array " + numbers[1]);
-            Console.WriteLine("Third value in numbers array " + numbers[2]);
-            Console.WriteLine();
-
-            string[] food = new string[5];
-            food[0] = "Pizza";
-            food[1] = "Hot Sauce";
-            Console.WriteLine("First value in names array " + food[0]);
-            Console.WriteLine(food[1]);
-
-            string[] namesArray = { "Archie", "Charlie", "Dad", "Mum", "Grandad" };
-            // position or index        0          1        2      3       4
-            Console.WriteLine("Second string in array: " + namesArray[1]);
-            Console.WriteLine("Fifth string in array: " + namesArray[4]);
-            Console.WriteLine();
-        }
-
-        private static void forEachLoops()
-        {
-            string[] namesArray = { "Archie", "Charlie", "Dad", "Mum", "Grandad" };
-            foreach (string name in namesArray)
-            {
-                Console.WriteLine("name: " + name);
+                Console.WriteLine(argValue);
             }
 
-            // 1. create a new array of 3 integers
-            // 2. assign the values of all 3 integers
-            // 3. write to the console the values of all 3 integers - using a foreach loop
+            Console.WriteLine("What is your name?");
 
-            int[] water = { 1, 52, 634, 23223, 99 };
+            string name = Console.ReadLine();
 
-            foreach (int liquid in water)
-            {
-                Console.Write(liquid + " ");
-            }
+            Console.WriteLine("Hello " + name);
 
-            // the new keyword creates a new reference in memory
-            string[] dadArray = new string[5];
-            string[] archieArray = dadArray; // archieArray is a reference to the same memory location as dadArray
-
-            dadArray[0] = "Cheeeeeeeese";
-            Console.WriteLine(archieArray[0]);
-            archieArray[1] = "Beans";
-            Console.WriteLine(dadArray[1]);
+            Thread.Sleep(3333);
         }
     }
 }
